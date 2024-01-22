f3b251f9d1caf59485b11ea09d7576790d09e890
crazyearn360 <157377640+crazyearn360@users.noreply.github.com>
2024-01-22 21:34:27 +0600

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,23 @@
+var title1 = "CashApp $750";
+var title2 = "Walmart $100";
+var title3 = "Google Play $100";
+var title4 = "Visa $500";
+var title5 = "Venmo $100";
+var title6 = "Paypal $750";
+var title7 = "Amazon $300";
+var title8 = "CashApp 2.0 $750";
+var title9 = "Credit Line $1000";
+var title10 = "McDonald's $200";
+var title11 = "Starbucks $150";
+
+var link1 = "https://smrturl.co/o/544335/53177516?s1=";
+var link2 = "https://smrturl.co/o/544335/1196338?s1=";
+var link3 = "https://smrturl.co/o/544335/53265173?s1=";
+var link4 = "https://smrturl.co/o/544335/53373285?s1=";
+var link5 = "https://smrturl.co/o/544335/53206664?s1=";
+var link6 = "https://smrturl.co/o/544335/53251150?s1=";
+var link7 = "https://smrturl.co/o/544335/53396479?s1=";
+var link8 = "https://smrturl.co/o/544335/53370715?s1=";
+var link9 = "https://smrturl.co/o/544335/53380771?s1=";
+var link10 = "https://smrturl.co/o/544335/53356127?s1=";
+var link11 = "https://smrturl.co/o/544335/53429098?s1=";
